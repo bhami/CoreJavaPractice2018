6e37809248178708c346cc1c934a889da84f55c1
Bhami <jainbhami@gmail.com>
2018-01-24 17:18:02 -0500

    Changed reverse string method

--- CoreJavaPractice2018/src/com/bhami/java/ReverseStringTest.java
+++ CoreJavaPractice2018/src/com/bhami/java/ReverseStringTest.java
@@ -8,19 +8,19 @@ public class ReverseStringTest {
 		System.out.println("Adding test1");
 		String inStr = "How are you";
 		
-		
+		/*
 		String[] newStr = inStr.split("\\s");
 		for(int i= newStr.length-1; i>=0; i--){
 			System.out.print(newStr[i] + " ");
-		}
+		}*/
 		
 		
 		
-		/*char[] strArr = inStr.toCharArray();
+		char[] strArr = inStr.toCharArray();
 		int len = strArr.length;
 		System.out.println("Input String: " + Arrays.toString(strArr));
 		for(int i= len-1; i>=0; i-- ){
 			System.out.print(strArr[i]);
-		}*/
+		}
 	}
 }
